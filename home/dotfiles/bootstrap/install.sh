9ee5d6155c652903b097c8b0ade078573f6f616d
Daan Schulpen <daan.schulpen@intraffic.nl>
2015-02-05 11:31:23 +0100

    bootstrap: added xclip to install

--- home/dotfiles/bootstrap/install.sh
+++ home/dotfiles/bootstrap/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo aptitude install build-essential gdb clang-3.5 subversion mercurial
+sudo aptitude install build-essential gdb clang-3.5 subversion mercurial xclip
 sudo aptitude build-dep vim-nox git clang-3.5
