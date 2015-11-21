8eb010fbeca1398d0bdd0b31519a573fdebc0e02
Daan Schulpen <daanschulpen@gmail.com>
2015-11-21 11:54:55 +0100

    clang: update to 3.7.0

--- home/dotfiles/bootstrap/clang.sh
+++ home/dotfiles/bootstrap/clang.sh
@@ -2,24 +2,26 @@
 mkdir -p $HOME/projects/other/llvm-src
 cd $HOME/projects/other/llvm-src
 #
-svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_351/final llvm
+svn co -q http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_370/final llvm
 #
 cd llvm/tools
-svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_351/final clang
+svn co -q http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_370/final clang
 cd ../..
 #
 cd llvm/tools/clang/tools
-svn co http://llvm.org/svn/llvm-project/clang-tools-extra/tags/RELEASE_351/final extra
+svn co -q http://llvm.org/svn/llvm-project/clang-tools-extra/tags/RELEASE_370/final extra
 cd ../../../..
 #
 cd llvm/projects
-svn co http://llvm.org/svn/llvm-project/compiler-rt/tags/RELEASE_351/final compiler-rt
+svn co -q http://llvm.org/svn/llvm-project/compiler-rt/tags/RELEASE_370/final compiler-rt
 cd ../..
 #
 cd llvm/projects
-svn co http://llvm.org/svn/llvm-project/test-suite/tags/RELEASE_351/final test-suite
+svn co -q http://llvm.org/svn/llvm-project/test-suite/tags/RELEASE_370/final test-suite
 cd ../..
 #
 mkdir -p build
 cd build
-../llvm/configure --prefix=/home/schuld/app/clang-3.5.1 --build=x86_64-pc-linux-gnu --enable-optimized && make && make install
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/schuld/app/clang-3.7.0 -G "Unix Makefiles" ../llvm
+make
+#../llvm/configure --prefix=/home/schuld/app/clang-3.7.0 --build=x86_64-pc-linux-gnu --enable-optimized && make && make install
