5f43414e9dd519637944c13e5cb9381f1de1e4a1
Daan Schulpen <daan.schulpen@intraffic.nl>
2019-02-02 15:53:07 +0100

    npm: added .prettierrc.js

--- home/dotfiles/bootstrap/init-npm-project.sh
+++ home/dotfiles/bootstrap/init-npm-project.sh
@@ -2,3 +2,4 @@
 npm init -y
 npm install -D eslint eslint-config-prettier eslint-plugin-prettier prettier flow-bin
 cp $HOME/dotfiles/bootstrap/.eslintrc.js .
+cp $HOME/dotfiles/bootstrap/.prettierrc.js .
