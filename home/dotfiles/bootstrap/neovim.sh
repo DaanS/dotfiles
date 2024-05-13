dbb08777a8a7a67125119574f13fb85f43e1a49f
Daan Schulpen <daan.schulpen@intraffic.nl>
2024-05-13 14:50:43 +0200

    prl: update for new vm teplate; stash some old configs

--- home/dotfiles/bootstrap/neovim.sh
+++ home/dotfiles/bootstrap/neovim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo yum install -y epel-release
-sudo yum install -y neovim python36
-sudo python36 -m ensurepip
-sudo python36 -m pip install --upgrade pip
-sudo python36 -m pip install neovim
+sudo yum install -y neovim
+sudo python3 -m ensurepip
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install neovim
 mkdir -p $HOME/projects/other
 cd $HOME/projects/other
 git clone http://github.com/powerline/fonts.git --depth=1
